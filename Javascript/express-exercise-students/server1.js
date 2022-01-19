f23f3cf2821f09d35d4afbe421bb1c1b8cd12481
Adrian Gonzalez <adriangonzalezfilgueira@gmail.com>
2022-01-19 07:59:14 +0100

    CodeWars Algoritmo

--- Javascript/express-exercise-students/server1.js
+++ Javascript/express-exercise-students/server1.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('<h1>El servidor funciona correctamente</h1>')
-})
+const express = require("express");
 
-app.listen(port, () => {
-   
-})
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    const html = `<h1>El servidor funciona correctamente</h1>
+    <p>Aquí iría el contenido del mensaje</p>`;
+
+    res.status(200).send(html);
+});
+
+try {
+    app.listen(PORT);
+    console.log(`Listening on http://localhost:${PORT}`);
+} catch (err) {
+    console.log(err);
+}
